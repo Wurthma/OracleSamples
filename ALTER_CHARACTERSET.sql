c45d021fde69bff3b52964bcdc88dd9b1e587f8d
George Henrique Wurthmann <gemakai@gmail.com>
2020-03-06 22:53:15 -0300

    Add code for correct problem for column size
    Add code for correct problem for column size when oracle use UTF8 NLS_CHARACTERSET. 
    Details: https://pt.stackoverflow.com/a/438855/35358

--- ALTER_CHARACTERSET.sql
+++ ALTER_CHARACTERSET.sql
@@ -17,3 +17,18 @@ or (if x64): Computador\HKEY_LOCAL_MACHINE\SOFTWARE\ORACLE\KEY_OraDb11g_home1
 Change NLS_LANG for: AMERICAN_AMERICA.WE8MSWIN1252 
 or (for brazilian): BRAZILIAN PORTUGUESE_BRAZIL.WE8MSWIN1252
 */
+
+
+--Code for alter session NLS_CHARACTERSET to consider CHAR for all columns (VARCHAR2(x CHAR)
+--Details: https://pt.stackoverflow.com/a/438855/35358
+declare
+    v_s_CharacterSet VARCHAR2(160);
+begin
+    select VALUE
+    into v_s_CharacterSet
+    from NLS_DATABASE_PARAMETERS
+    where PARAMETER = 'NLS_CHARACTERSET';
+    if v_s_CharacterSet in ('UTF8', 'AL32UTF8') then
+        execute immediate 'alter session set nls_length_semantics=char';
+    end if;
+end;
